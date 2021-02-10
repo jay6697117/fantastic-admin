4f2f54010bfb57d051bda943456d84be8f60a254
steve zhang <871506263@qq.com>
2021-02-10 21:50:19 +0800

    提交代码于: 2021-02-10 21:50:19

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,11 @@ export default {
       this.$store.commit('settings/setMode', document.body.clientWidth);
     };
     window.onresize();
+    console.log('App mounted 1');
+    console.log('App mounted 2');
+    console.log('App mounted 3');
+    console.log('App mounted 4');
+    console.log('App mounted 5');
   },
   methods: {},
   metaInfo() {
@@ -49,6 +54,6 @@ export default {
 
 <style scoped>
 #app {
-  height: 100%;
+    height: 100%;
 }
 </style>
