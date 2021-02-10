f76637d5d783df1ad50b16ebb5ee6ae5b1a14ac9
steve zhang <871506263@qq.com>
2021-02-10 21:45:54 +0800

    提交代码于: 2021-02-10 21:45:53

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,16 @@
+// const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
+
+// const consoleRemovePlugins = [];
+// if (IS_PROD) {
+//   consoleRemovePlugins.push('transform-remove-console');
+// }
+
 module.exports = {
-    presets: [
-        '@vue/cli-plugin-babel/preset'
-    ],
-    env: {
-        development: {
-            plugins: ['dynamic-import-node']
-        }
+  presets: ['@vue/cli-plugin-babel/preset'],
+  // consoleRemovePlugins,
+  env: {
+    development: {
+      plugins: ['dynamic-import-node']
     }
-}
+  }
+};
