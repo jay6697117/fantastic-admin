73a131ff1abebb311abb6f2aa3b631d55bcd6c26
steve zhang <871506263@qq.com>
2021-02-10 22:24:30 +0800

    提交代码于: 2021-02-10 22:24:30

--- src/main.js
+++ src/main.js
@@ -1,46 +1,46 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-import api from './api'
-Vue.prototype.$api = api
+import api from './api';
+Vue.prototype.$api = api;
 
-import dayjs from 'dayjs'
-Vue.prototype.$dayjs = dayjs
+import dayjs from 'dayjs';
+Vue.prototype.$dayjs = dayjs;
 
-import auth from './util/auth'
-Vue.use(auth)
+import auth from './util/auth';
+Vue.use(auth);
 
-import cookies from 'vue-cookies'
-Vue.use(cookies)
+import cookies from 'vue-cookies';
+Vue.use(cookies);
 
-import VueMeta from 'vue-meta'
-Vue.use(VueMeta)
+import VueMeta from 'vue-meta';
+Vue.use(VueMeta);
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
-import hotkeys from 'hotkeys-js'
-Vue.prototype.$hotkeys = hotkeys
+import hotkeys from 'hotkeys-js';
+Vue.prototype.$hotkeys = hotkeys;
 
 // 全局组件自动注册
-import './components/autoRegister'
+import './components/autoRegister';
 
 // 自动加载 svg 图标
-const req = require.context('./assets/icons', false, /\.svg$/)
-const requireAll = requireContext => requireContext.keys().map(requireContext)
-requireAll(req)
+const req = require.context('./assets/icons', false, /\.svg$/);
+const requireAll = requireContext => requireContext.keys().map(requireContext);
+requireAll(req);
 
-import './assets/styles/reset.scss'
+import './assets/styles/reset.scss';
 
-import './mock'
+import './mock';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.prototype.$eventBus = new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
